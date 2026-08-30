6227eeea112381e41edd5bee9709753260e496cb
1337hub1337 <1337hub1337@gmail.com>
2026-08-30 16:25:04 +0700

    loader.lua

--- loader.lua
+++ loader.lua
@@ -9,7 +9,7 @@ local GAMES = {
 	[72042130041700] = { "Throw a Coin", "https://api.jnkie.com/api/v1/luascripts/public/07b7ae726719e5de5522a393043310a12e8d057a688846430d01705f9c07bf87/download", "KEYLESS" },
 	[83038462357724] = { "Dig & Clean", "https://api.jnkie.com/api/v1/luascripts/public/6ad58afa8a8dc632299c069f5718afc31d2d1f45c940a114e54340e2a9e57304/download", "KEYLESS" },
 	[107778070777162] = { "Steal An Egg", "https://api.jnkie.com/api/v1/luascripts/public/a8ad4dd6a9cac6965c930d04a43d2b433af52e2a4fb84f6e89a20c88165a4771/download", "KEYLESS" },
-	[94640181989498] = { "Grow a Chicken Fighter", "https://gist.githubusercontent.com/1337hub1337/88bc394dd8f606327d185db53d66b85e/raw/731de54cd543a820e2b19bd75cd2644d74ad773d/gacf.lua", nil },
+	[94640181989498] = { "Grow a Chicken Fighter", "https://gist.githubusercontent.com/1337hub1337/3b8ab9a79b172a8cfac741e1943e2e32/raw/208c0edc24182c7b8fd95c43eaf783a6a6c85a2b/gacf.lua", nil },
 }
 
 local currentGame = GAMES[game.PlaceId]
